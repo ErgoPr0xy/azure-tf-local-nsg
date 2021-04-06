fc1711d889a0fa72f89e840d9b8c7be74df1eb49
ErgoPr0xy <slive77@gmail.com>
2021-04-06 21:23:51 +0000

    updated variables

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "prefix" {
+    type = string
+    default = "ex-kube"
+}
+
+variable "location" {
+    type = string
+    default = "westeurope"
+}
+
+variable "aks_subnet_name" {
+  description = "AKS Subnet Name."
+  default     = "subnet-dev-westeurope"
+}
+
+variable "password" {
+  description = "Service Principal password"
+  default = "YourPasswordHereCapitalWhatever!#"
+}
